c03bfddc552635bc6520f544661385c60bfd6723
Maha-Rabah <maharabah13@gmail.com>
2024-10-27 00:53:17 +0300

    sec edit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -93,7 +93,7 @@ function NavBar() {
           
             <Nav.Item className="fork-btn">
               <Button
-                href=""
+                href="https://github.com/Maha-Rabah/My-Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
